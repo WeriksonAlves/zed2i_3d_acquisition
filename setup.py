8287bb6df7240228b97aedc69c06f5e1aafb0e69
weriksonfoalves <werikson.alves@dt-labs.ai>
2025-12-10 02:13:18 -0300

    Initial ROS2 package skeleton for ZED2i data acquisition

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'zed2i_data_acquisition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='werikson',
+    maintainer_email='werikson.alves@dt-labs.ai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
